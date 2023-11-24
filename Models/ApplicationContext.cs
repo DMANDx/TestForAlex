3c1247147071e95b27ddd0ee6768de157865ac16
DMAND <123652570+DMANDx@users.noreply.github.com>
2023-11-24 10:12:57 +0300

    Add files via upload

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Avto1Test.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Url> Urls { get; set; } = null!;
+        
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+           // Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Url>()
+                .HasIndex(p => new { p.MainURL, p.TinyURL})
+                .IsUnique(true);
+        }
+    }
+}
---
+++ Models/Url.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Avto1Test.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Url(ErrorMessage = "Некорректный URL")]        
+        [Display(Name = "URL")]
+        public string? MainURL { get; set; }
+        [StringLength(1000, MinimumLength = 9, ErrorMessage = "Минимум 9 символов")]
+        [Display(Name = "сокращенный URL")]
+        public string? TinyURL { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Дата создания")]
+        public DateTime DateCreate { get; set; }
+        [Display(Name = "кол-во кликов")]
+        public int NumOfCall { get; set; }
+    }
+}
