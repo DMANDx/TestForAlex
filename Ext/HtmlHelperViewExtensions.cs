728b21999f5e46aa7a2f2bcf00235a8129a4afa1
DMAND <123652570+DMANDx@users.noreply.github.com>
2023-11-24 10:13:32 +0300

    Add files via upload

---
+++ Ext/HtmlHelperViewExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace Avto1Test.Ext
+{
+    //[ViewComponent]
+    //public class HtmlUriViewComponent
+    //{                
+    //        public IViewComponentResult Invoke(string str)            
+    //        {
+    //            Uri uri = new(str);
+    //            string lastSegment = uri.Segments[uri.Segments.Length - 1];
+    //            return lastSegment;
+    //        }        
+    //}
+}
