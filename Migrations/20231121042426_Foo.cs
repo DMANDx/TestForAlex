435be6cce29c57232315ce3d14dbf1a79887dc1c
DMAND <123652570+DMANDx@users.noreply.github.com>
2023-11-24 10:13:18 +0300

    Add files via upload

---
+++ Migrations/20231121042426_Foo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Avto1Test.Migrations
+{
+    public partial class Foo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    MainURL = table.Column<string>(type: "varchar(255)", nullable: false),
+                    TinyURL = table.Column<string>(type: "varchar(255)", nullable: true),
+                    DateCreate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    NumOfCall = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_MainURL_TinyURL",
+                table: "Urls",
+                columns: new[] { "MainURL", "TinyURL" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
