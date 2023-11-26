f657a8ab5a01d8db81d8bbc380629271b2fa908e
DMAND <dmandxist@gmail.com>
2023-11-26 15:03:41 +0300

    Update Url.cs

--- Models/Url.cs
+++ Models/Url.cs
@@ -10,12 +10,15 @@ namespace Avto1Test.Models
         [Url(ErrorMessage = "Некорректный URL")]        
         [Display(Name = "URL")]
         public string? MainURL { get; set; }
-        [StringLength(1000, MinimumLength = 9, ErrorMessage = "Минимум 9 символов")]
+
+        [StringLength(255, MinimumLength = 9, ErrorMessage = "Минимум 9 символов")]
         [Display(Name = "сокращенный URL")]
         public string? TinyURL { get; set; }
+
         [DataType(DataType.DateTime)]
         [Display(Name = "Дата создания")]
         public DateTime DateCreate { get; set; }
+
         [Display(Name = "кол-во кликов")]
         public int NumOfCall { get; set; }
     }
