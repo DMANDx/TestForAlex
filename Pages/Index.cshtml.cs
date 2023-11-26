0ad7912a013f327acce3fe1661c564518de24958
DMAND <dmandxist@gmail.com>
2023-11-26 15:30:10 +0300

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Avto1Test.Models;
 using System.Security.Policy;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.Extensions.Logging;
 
 namespace Avto1Test.Pages
 {
@@ -15,6 +16,10 @@ namespace Avto1Test.Pages
     public class IndexModel : PageModel
     {
         private readonly Avto1Test.Models.ApplicationContext _context;
+
+        static ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = loggerFactory.CreateLogger<IndexModel>();
+
 
         public IndexModel(Avto1Test.Models.ApplicationContext context)
         {
@@ -24,11 +29,20 @@ namespace Avto1Test.Pages
         public new IList<Models.Url> Url { get;set; } = default!;
 
         public async Task OnGetAsync()
-        {
-            if (_context.Urls != null)
+        {            
+            try
             {
-                Url = await _context.Urls.OrderByDescending(z => z.DateCreate).ToListAsync();
+                if (_context.Urls != null)
+                {
+                    Url = await _context.Urls.OrderByDescending(z => z.DateCreate).ToListAsync();
+                }
             }
+            catch (Exception ex) 
+            {
+                logger.LogWarning($"Getting all:ERROR {ex}");
+            }
+
+            logger.LogInformation("Getting all:OK");
         }
 
         //CORSES!!!
@@ -48,6 +62,13 @@ namespace Avto1Test.Pages
         //    //return Redirect(ur.MainURL);
         //}       
 
+
+
+        /// <summary>
+        /// Encrement for link
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> OnPostIncrem(int id)
         {          
             var ur = await _context.Urls.Where(z => z.Id == id).FirstOrDefaultAsync();
@@ -56,14 +77,22 @@ namespace Avto1Test.Pages
             {
                 ur.NumOfCall += 1;
                 _context.Attach(ur).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-            }            
-            
+                await _context.SaveChangesAsync();                
+            }
+            else
+            {
+                logger.LogCritical("OnPostIncrem :ERROR ur = null");
+            }
+                                            
             //return RedirectToPage("");
             return Redirect(ur.MainURL);
-            //return null;
         }
 
+        /// <summary>
+        /// Function for getting Tiny link
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public string GetUri(string str)
         {
             Uri uri = new(str);
