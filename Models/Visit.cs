02dba41aad2a181ad70758dcbc7d3b960bd7e353
DMAND <dmandxist@gmail.com>
2024-03-29 15:51:57 +0300

    Create Visit.cs

---
+++ Models/Visit.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Avto1Test.Models
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public string? UserAgent { get; set; }
+        public string? RemoteAddr { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
